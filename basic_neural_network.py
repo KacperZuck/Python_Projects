4cda1d6712d3ce3a0111c674272ba536bd33d468
Kacper <163001822+KacperZuck@users.noreply.github.com>
2026-04-09 13:22:14 +0200

    Add files via upload
    basic nn without libs

---
+++ basic_neural_network.py
@@ -0,0 +1,19 @@
+inputs = [ 1, 2, 3, 2.5]
+
+weights = [
+    [ 0.2, 0.8, -0.5, 1.0],
+    [ 0.5, -0.91, 0.26, -0.5],
+    [ -0.26, -0.27, 0.17, 0.87]]
+
+bias = [ 2, 3, 0.5]
+
+output = []
+
+for n_weight, n_bias in zip(weights, bias):
+    n_output = 0
+    for n_input, weight in zip(inputs, n_weight):
+        n_output += n_input * weight
+    n_output += n_bias
+    output.append(n_output)
+
+print(output)
